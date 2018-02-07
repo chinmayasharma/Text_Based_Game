62c76cd8ed5ab574d4f1219b028d8d0512bed05b
Chinmaya Sharma <chinmaya294@outlook.com>
2018-02-06 23:35:28 -0600

    Rearranged methods and changed comments

--- src/Layout.java
+++ src/Layout.java
@@ -1,7 +1,13 @@
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class Layout {
   @SerializedName("startingRoom")
@@ -11,7 +17,7 @@ public class Layout {
   private String endingRoom;
 
   @SerializedName("rooms")
-  private ArrayList<Room> rooms = null;
+  private ArrayList<Room> rooms = new ArrayList<>();
 
   /** @return name of starting room */
   public String getStartingRoom() {
@@ -26,6 +32,31 @@ public class Layout {
   /** @return list of rooms */
   public ArrayList<Room> getRooms() {
     return rooms;
+  }
+
+  /**
+   * @param url link to access file from
+   * @return ojejct of type layout
+   * @throws UnirestException library exception thrown
+   * @throws MalformedURLException thrown if URL is invalid
+   */
+  public static Layout makeApiRequest(String url) throws UnirestException, MalformedURLException {
+
+    final HttpResponse<String> stringHttpResponse;
+
+    // This will throw MalformedURLException if the url is malformed.
+    new URL(url);
+
+    stringHttpResponse = Unirest.get(url).asString();
+    // Check to see if the request was successful; if so, convert the payload JSON into Java objects
+    Layout layoutMap = null;
+
+    if (stringHttpResponse.getStatus() == AdventureConstants.STATUS_OK) {
+      String json = stringHttpResponse.getBody();
+      Gson gson = new Gson();
+      layoutMap = gson.fromJson(json, Layout.class);
+    }
+    return layoutMap;
   }
 
   /**
