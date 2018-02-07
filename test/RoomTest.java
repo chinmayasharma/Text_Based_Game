dd33c4d9a837281816f159f172a693da5733f0e5
Chinmaya Sharma <chinmaya294@outlook.com>
2018-02-06 23:33:04 -0600

    Added comments

--- test/RoomTest.java
+++ test/RoomTest.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,45 +8,39 @@ import static org.junit.Assert.*;
 
 public class RoomTest {
 
+  private static Gson gson = new Gson();
+  public static Layout expectedLayout =
+      gson.fromJson(AdventureConstants.getFileContentsAsString("siebel.json"), Layout.class);
+
   @Before
   public void setUp() throws Exception {}
 
-  /**
-   * Checks if name of first room is same as expected
-   */
+  /** Checks if name of first room is same as expected */
   @Test
   public void getName() {
-    assertEquals("MatthewsStreet", GetJsonFileTest.expectedLayout.getRooms().get(0).getName());
+    assertEquals("MatthewsStreet", expectedLayout.getRooms().get(0).getName());
   }
 
-  /**
-   * Checks if name of description of first room is same as expected
-   */
+  /** Checks if name of description of first room is same as expected */
   @Test
   public void getDescription() {
     assertEquals(
         "You are on Matthews, outside the Siebel Center",
-        GetJsonFileTest.expectedLayout.getRooms().get(0).getDescription());
+        expectedLayout.getRooms().get(0).getDescription());
   }
 
-  /**
-   *Checks if name of item in first room is same as expected
-   */
+  /** Checks if name of item in first room is same as expected */
   @Test
   public void getItems() {
     ArrayList<String> itemList = new ArrayList<>();
     itemList.add("coin");
-    assertEquals(itemList, GetJsonFileTest.expectedLayout.getRooms().get(0).getItems());
+    assertEquals(itemList, expectedLayout.getRooms().get(0).getItems());
   }
 
-  /**
-   * Checks if name of direction in first room is same as expected
-   */
-
+  /** Checks if name of direction in first room is same as expected */
   @Test
   public void getDirections() {
     assertEquals(
-        "East",
-        GetJsonFileTest.expectedLayout.getRooms().get(0).getDirections().get(0).getDirectionName());
+        "East", expectedLayout.getRooms().get(0).getDirections().get(0).getDirectionName());
   }
 }
