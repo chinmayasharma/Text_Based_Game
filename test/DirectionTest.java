bcfb335198be08fe22a6489dcf25b4a94f3c5cc5
Chinmaya Sharma <chinmaya294@outlook.com>
2018-02-13 16:58:49 -0600

    Added tests and comments

--- test/DirectionTest.java
+++ test/DirectionTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class DirectionTest {
 
   private static Gson gson = new Gson();
-  public static Layout expectedLayout =
+  private static Layout expectedLayout =
       gson.fromJson(AdventureConstants.getFileContentsAsString("siebel.json"), Layout.class);
 
   /** Checks if direction name returned is same as expected */
@@ -19,6 +19,6 @@ public class DirectionTest {
   /** Checks if name of room returned y getRoom method is the same as expected */
   @Test
   public void getRoom() {
-    assertEquals("MatthewsStreet", expectedLayout.getRooms().get(0).getName());
+    assertEquals("Champaign", expectedLayout.getRooms().get(0).getName());
   }
 }
