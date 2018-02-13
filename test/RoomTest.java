d4a04b27ac2354972a8ee2f41c105eab7026b1df
Chinmaya Sharma <chinmaya294@outlook.com>
2018-02-13 16:56:35 -0600

    Added tests and comments

--- test/RoomTest.java
+++ test/RoomTest.java
@@ -1,40 +1,92 @@
 import com.google.gson.Gson;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.*;
 
 public class RoomTest {
 
   private static Gson gson = new Gson();
-  public static Layout expectedLayout =
+  public static Layout layout =
       gson.fromJson(AdventureConstants.getFileContentsAsString("siebel.json"), Layout.class);
+
+  public Adventure testGame = new Adventure(layout);
 
   /** Checks if name of first room is same as expected */
   @Test
   public void getName() {
-    assertEquals("MatthewsStreet", expectedLayout.getRooms().get(0).getName());
+    assertEquals("Champaign", layout.getRooms().get(0).getName());
   }
 
   /** Checks if name of description of first room is same as expected */
   @Test
   public void getDescription() {
     assertEquals(
-        "You are on Matthews, outside the Siebel Center",
-        expectedLayout.getRooms().get(0).getDescription());
+        "You are in Champaign, IL, USA. You're in the middle of nowhere, so you better start moving",
+        layout.getRooms().get(0).getDescription());
   }
 
   /** Checks if name of item in first room is same as expected */
   @Test
   public void getItems() {
-    assertEquals("coin", expectedLayout.getRooms().get(0).getItems().get(0).getName());
+    assertEquals("Textbook", layout.getRooms().get(0).getItems().get(0).getName());
   }
 
   /** Checks if name of direction in first room is same as expected */
   @Test
   public void getDirections() {
-    assertEquals(
-        "East", expectedLayout.getRooms().get(0).getDirections().get(0).getDirectionName());
+    assertEquals("East", layout.getRooms().get(0).getDirections().get(0).getDirectionName());
+  }
+
+  /**
+   * *********************************************************************************************************
+   * String output for checking items in room
+   * *********************************************************************************************************
+   */
+  @Test
+  public void checkItemTest() {
+    assertTrue(layout.getRooms().get(0).checkItem("Textbook"));
+  }
+
+  /** checks if item exists in the itemList, if invalid */
+  @Test
+  public void findInvalidItemTest() {
+    assertNull(layout.getRooms().get(0).findItem("can"));
+  }
+
+  /** checks if item exists in the itemList, if null */
+  @Test
+  public void findNullItemTest() {
+    assertNull(layout.getRooms().get(0).findItem(null));
+  }
+
+  /** checks if item exists in the itemList, if empty */
+  @Test
+  public void findEmptyStringItemTest() {
+    assertNull(layout.getRooms().get(0).findItem(""));
+  }
+
+
+  /**
+   * *********************************************************************************************************
+   * String output for checking direction
+   * *********************************************************************************************************
+   */
+
+  /** checks if direction exists in the empty directionList if empty */
+  @Test
+  public void emptyDirectionTest() {
+    assertFalse(layout.getRooms().get(0).checkDirection(""));
+  }
+
+  /** checks if direction exists in the empty directionList if invalid */
+  @Test
+  public void checkInvalidDirectionTest() {
+    assertFalse(layout.getRooms().get(0).checkDirection("Down"));
+  }
+
+  /** checks if direction exists in the empty directionList if null */
+  @Test
+  public void checkNullDirectionTest() {
+    assertFalse(layout.getRooms().get(0).checkDirection(null));
   }
 }
