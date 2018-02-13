4d65e7be9c418c106203b74642bdbfc3d9a10c8a
Chinmaya Sharma <chinmaya294@outlook.com>
2018-02-13 16:58:32 -0600

    Added tests and comments

--- test/ItemTest.java
+++ test/ItemTest.java
@@ -1,14 +1,24 @@
+import com.google.gson.Gson;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ItemTest {
 
+  private static Gson gson = new Gson();
+  private static Layout expectedLayout =
+          gson.fromJson(AdventureConstants.getFileContentsAsString("siebel.json"), Layout.class);
+
   @Test
   public void getName() {
-
+    assertEquals(
+          "Textbook", expectedLayout.getRooms().get(0).getItems().get(0).getName());
   }
 
   @Test
-  public void getDamage() {}
+  public void getDamage() {
+    assertEquals(
+            10.0, expectedLayout.getRooms().get(0).getItems().get(0).getDamage(), 0.01);
+
+  }
 }
